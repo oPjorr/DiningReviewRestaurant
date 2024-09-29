8762a6892cfc6b5b253c08ecc5d531f880782270
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-09-29 18:48:38 -0300

    fix - cors urls

--- back-end/DiningReviewRestaurant/settings.py
+++ back-end/DiningReviewRestaurant/settings.py
@@ -152,5 +152,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",  # URL do front-end
-    'https://diningreviewrestaurant-api.onrender.com',
 ]
+
+CSRF_TRUSTED_ORIGINS = ['https://diningreviewrestaurant-api.onrender.com']
