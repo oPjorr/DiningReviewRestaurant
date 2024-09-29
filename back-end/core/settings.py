880b13e27c86efc648210324f4354b99d2528b26
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-09-29 20:40:18 -0300

    fix - settings.py

--- back-end/core/settings.py
+++ back-end/core/settings.py
@@ -152,5 +152,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
-    'https://diningreviewrestaurant-api.onrender.com',
 ]
