520bb2112064059006fdc1e6912b4a52edf0854a
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-09-29 21:07:48 -0300

    fix - sqlite

--- back-end/core/settings.py
+++ back-end/core/settings.py
@@ -94,10 +94,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/mysite',
-            conn_max_age=600
-        )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
